68088f384f11e0e8c8e551cf977ab887f8851026
Louis Borsu <sat@satprod.net>
2014-12-14 13:04:27 +0100

    Cleaning

--- server.js
+++ server.js
@@ -2,13 +2,6 @@
 var debug = require('debug')('dontgetboard');
 var app = require('./app');
 
-// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
-
-// var server = app.listen(app.get('port'), function() {
-//   debug('Express server listening on port ' + server.address().port);
-// });
-
-
 // Start the server.
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
  	ip   = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
