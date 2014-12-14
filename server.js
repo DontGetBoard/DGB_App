fd39192063fffb928d07cdd8f5a8a2dae662c08f
Louis Borsu <sat@satprod.net>
2014-12-14 12:31:22 +0100

    Modifications to works on openshift

--- server.js
+++ server.js
@@ -1,83 +1,9 @@
-require('newrelic');
+#!/usr/bin/env node
+var debug = require('debug')('dontgetboard');
+var app = require('./app');
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+app.set('port', process.env.PORT || 3000);
 
-// All MongoDB Related Stuff
-
-var app = express();
-
-if (app.get('env') === 'development') {
-    mongoose.connect('mongodb://localhost/dontgetboard');
-}else{
-    mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL;
-    mongo_app_name = process.env.OPENSHIFT_APP_NAME;
-    mongoose.connect(mongo_url+mongo_app_name);
-}
-require('./models/Games');
-
-var routes = require('./routes/index');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
-
-// Requires meanio
-var mean = require('meanio');
-
-// Creates and serves mean application
-mean.serve({ /*options placeholder*/ }, function(app, config) {
-  console.log('Mean app started on port ' + config.http.port + ' (' + process.env.NODE_ENV + ')');
-  if(config.https && config.https.port){
-    console.log('Mean secure app started on port ' + config.https.port + ' (' + process.env.NODE_ENV + ')');
-  }
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
 });
