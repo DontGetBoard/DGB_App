f813edc1c35e52cac998b4ddd2237b1f7e267d12
Louis Borsu <sat@satprod.net>
2014-12-14 12:45:12 +0100

    Modifications to works on openshift 2

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 var debug = require('debug')('dontgetboard');
 var app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
+// var server = app.listen(app.get('port'), function() {
+//   debug('Express server listening on port ' + server.address().port);
+// });
+
+
+// Start the server.
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+ 	ip   = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+app.listen(port, ip);
