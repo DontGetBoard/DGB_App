9ad446b2540cc0e9478f6d7369809135c850c239
Louis Borsu <sat@satprod.net>
2015-05-10 21:05:39 +0200

    Cleaning

--- config/passport.js
+++ config/passport.js
@@ -1,62 +1,41 @@
 'use strict';
 
-// load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
 
 var config = require('config');
 
-// Resources
 var mailGun = require('../src/resources/mailGun');
 var mc      = require('../src/resources/mailChimp');
 
-// load up the user model
 var mongoose = require('mongoose');
 var User     = mongoose.model('User');
 
-// expose this function to our app using module.exports
 module.exports = function (passport, gravatar) {
 
-  // =========================================================================
-  // passport session setup ==================================================
-  // =========================================================================
-  // required for persistent login sessions
-  // passport needs ability to serialize and unserialize users out of session
-
-  // used to serialize the user for the session
   passport.serializeUser(function (user, done) {
     done(null, user.id);
   });
 
-  // used to deserialize the user
   passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
       done(err, user);
     });
   });
 
-  // =========================================================================
-  // LOCAL SIGNUP ============================================================
-  // =========================================================================
   passport.use('local-signup', new LocalStrategy({
     usernameField : 'email',
     passwordField : 'password',
     passReqToCallback : true
   }, function (req, email, password, done) {
 
-    // asynchronous
-    // User.findOne wont fire unless data is sent back
     process.nextTick(function () {
 
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
       User.findOne({ 'local.email' :  email }, function (err, user) {
-        // if there are any errors, return the error
         if (err) {
 
           return done(err);
         }
 
-        // check to see if theres already a user with that email
         if (user) {
 
           return done(
@@ -66,24 +45,19 @@ module.exports = function (passport, gravatar) {
           );
         } else {
 
-          // if there is no user with that email
-          // create the user
           var newUser            = new User();
 
-          // set the user's local credentials
           newUser.avatar         = gravatar.imageUrl(email);
           newUser.local.email    = email;
           newUser.local.password = newUser.generateHash(password);
           newUser.local.username = req.body.username;
 
-          // save the user
           newUser.save(function (err) {
             if (err) {
 
               return done(err);
             }
 
-            // Sending Welcome Email
             var data = {
               from: 'Don\'t get board <no-reply@mg.dontgetboard.net>',
               to: email,
