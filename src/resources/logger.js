a29b7e72caa8da9d7f2a691e96a7d268ff3abeba
Louis Borsu <sat@satprod.net>
2015-06-07 15:09:59 +0200

    Logging

--- src/resources/logger.js
+++ src/resources/logger.js
@@ -8,18 +8,12 @@ var LOGGER_NAME = config.loggerName;
 
 var ENV = process.env.NODE_ENV;
 
-if (ENV === 'production') {
-  module.exports = logentries.logger({
-    token: config.logEntries.token
-  });
-} else {
-  module.exports  = bunyan.createLogger({
-    name: LOGGER_NAME,
-    serializers: {
-      error: bunyan.stdSerializers.err
-    }
-  });
-}
+module.exports  = bunyan.createLogger({
+  name: LOGGER_NAME,
+  serializers: {
+    error: bunyan.stdSerializers.err
+  }
+});
 
 
 
